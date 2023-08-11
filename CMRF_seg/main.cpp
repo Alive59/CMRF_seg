ca3498a096565591872084ecef9bc125000619ba
Alive59 <6AliveLiew@users.noreply.github.com>
2023-08-11 11:19:27 +0900

    Initial Commit

---
+++ CMRF_seg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CMRF_seg
+//
+//  Created by Alive on 2023/8/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
